a0c618369f561a0d5c1923d7f2ce12e67c554913
darth-218 <yahia.hany.gaber@proton.me>
2024-07-06 02:12:49 +0300

    Started working on creating new sessions

--- Python/t-session/tsession.py
+++ Python/t-session/tsession.py
@@ -3,20 +3,38 @@
 import curses
 from curses import wrapper, newwin
 
+
 def main(stdscr):
-    curses.noecho()
+
+    curses.cbreak()
     stdscr.clear()
 
-    main_win_height = 20
-    main_win_width  = 40
-    main_win_y = 10
-    main_win_x = 20
+    main_win_height = int(curses.LINES * 0.8)
+    main_win_width  = int(curses.COLS * 0.5)
+    main_win_y = int((curses.LINES - main_win_height) * 0.5)
+    main_win_x = int((curses.COLS - main_win_width) * 0.5)
+
+    sessions_win_height = int(main_win_height * 0.3)
+    sessions_win_width  = int(main_win_width * 0.95)
+    sessions_win_y = int((curses.LINES - sessions_win_height) * 0.83)
+    sessions_win_x = int((curses.COLS - sessions_win_width) * 0.5)
+
     main_win = newwin(main_win_height, main_win_width, main_win_y, main_win_x)
+    main_win.box()
 
-    main_win.addstr(3, 3, "Test window")
+    sessions_win = newwin(sessions_win_height, sessions_win_width, sessions_win_y, sessions_win_x)
+    sessions_win.box()
+
+    stdscr.refresh()
     main_win.refresh()
+    sessions_win.refresh()
 
-    stdscr.getkey()
+    while 1:
+        keypress = stdscr.getkey()  # noqa: E701
+        if keypress == curses.KEY_ENTER:
+            pass
 
 wrapper(main)
 
+def createSession(starting_directory: str):
+    new_session_command = ["tmux", "new-session", "-c", starting_directory, "-s", starting_directory.split()[-1]]
