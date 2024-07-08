4220c598ccc5eb3cd31ed3e179fa3e12a3ad5fc0
darth-218 <yahia.hany.gaber@proton.me>
2024-07-08 18:23:12 +0300

    More work on the TUI and started working on commands

--- Python/t-session/tsession.py
+++ Python/t-session/tsession.py
@@ -2,51 +2,64 @@
 
 # TODO: Command exit codes
 
-import curses, os, subprocess
+import curses, subprocess
 from curses import wrapper, newwin
+
 
 def main(stdscr):
 
-    curses.cbreak() 
+    curses.cbreak()
     curses.curs_set(0)
     stdscr.clear()
 
     main_win_height = int(curses.LINES * 0.5)
-    main_win_width  = int(curses.COLS * 0.5)
+    main_win_width = int(curses.COLS * 0.5)
     main_win_y = int(curses.LINES * 0.5) - int(main_win_height * 0.5)
     main_win_x = int(curses.COLS * 0.5) - int(main_win_width * 0.5)
 
     commands_win_height = int(main_win_height * 0.2)
-    commands_win_width  = main_win_width
+    commands_win_width = main_win_width
     commands_win_y = int(main_win_y + (main_win_height - main_win_height * 0.2)) + 1
     commands_win_x = int(curses.COLS * 0.5) - int(commands_win_width * 0.5)
 
-    commands_win = newwin(commands_win_height, commands_win_width, commands_win_y, commands_win_x)
+    commands_win = newwin(
+        commands_win_height, commands_win_width, commands_win_y, commands_win_x
+    )
     commands_win.box()
 
     main_win = newwin(main_win_height, main_win_width, main_win_y, main_win_x)
     main_win.box()
 
     printBinds(commands_win, commands_win_width)
-
-    sessions = getSessions()
-
-    # session_x = int((len(max(sessions)) - main_win_width) / 2)
-    session_y = 1
-    session_x = 1
-
-    for i in sessions:
-        main_win.addstr(session_y, session_x, i)
-        session_y += 2
+    printSessions(main_win, main_win_width)
 
     stdscr.refresh()
     main_win.refresh()
     commands_win.refresh()
 
     while 1:
+        commands = {
+            "q": "quit()",
+            "n": "New session",
+            "c": f"createSession('{subprocess.check_output("pwd", shell = False).decode()}')",
+            "K": "killSession(" ")",
+        }
         keypress = stdscr.getkey()
-        if keypress == "q":
-            quit()
+        if keypress in commands:
+            eval(commands[keypress])
+            printSessions(main_win, main_win_width)
+            main_win.refresh()
+
+
+def printSessions(window, width) -> None:
+    sessions = getSessions()
+    session_x = int((width - getMaxlen(sessions)) / 2)
+    session_y = 1
+    for i in sessions:
+        window.addstr(session_y, session_x, i)
+        session_y += 2
+    # window.refresh()
+
 
 def printBinds(window, width) -> None:
     bindings = ["q: Quit", "n: New session", "c: Create session", "k: Kill session"]
@@ -57,27 +70,44 @@ def printBinds(window, width) -> None:
     window.addstr(2, y, bindings[3])
 
 
+def getMaxlen(sessions: list[str]) -> int:
+    maxlen = 0
+    for i in sessions:
+        maxlen = maxlen if len(i) < maxlen else len(i)
+    return maxlen
+
+
 def getSessions() -> list[str]:
     list_command = ["tmux", "list-sessions"]
-    sessions = subprocess.check_output(list_command, shell = False)
+    sessions = subprocess.check_output(list_command, shell=False)
     sessions = sessions.decode()
     sessions = sessions.split("\n")
     return sessions
 
 
-def createSession(starting_directory: str, session_name = None) -> None:
-    session_name = starting_directory.split()[-1] if session_name == None else session_name
-    new_session_command = ["tmux", "new-session", "-c", starting_directory, "-s", session_name]
-
+def createSession(starting_directory: str, session_name=None) -> None:
+    session_name = (
+        starting_directory.split()[-1] if session_name == None else session_name
+    )
+    new_session_command = [
+        "tmux",
+        "new-session",
+        "-A",
+        "-d",
+        "-c",
+        starting_directory,
+        "-s",
+        session_name,
+    ]
     try:
-        os.system(" ".join(new_session_command))
-
-    except: # FIX: Better error handling
-        print("Error creating session")
+        subprocess.run(new_session_command)
+    except:  # FIX: Better error handling
+        pass
 
 
 def killSession(session_name: str) -> None:
     kill_session_command = ["tmux", "kill-session", "-t", session_name]
-    os.system(" ".join(kill_session_command))
+    subprocess.run(kill_session_command)
+
 
 wrapper(main)
